05ead3363c11be5e3f2ba0a688e17edd479f9bf3
ManavLohabade <manavlohabade26@gmail.com>
2025-06-09 12:29:31 +0530

    Refactor: Improve prediction accuracy

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
 import pickle
 import numpy as np
 from flask import Flask, render_template, request, jsonify
+import re
 
 app = Flask(__name__)
 
 # Load the trained model
 model = pickle.load(open('models/finalized_model.pkl', 'rb'))
+vectorizer = pickle.load(open('vectorizer.pkl', 'rb'))
+
+def preprocess_text(text):
+    text = text.lower()
+    text = re.sub(r'\[.*?\]', '', text) # Remove text in square brackets
+    text = re.sub(r'\W', ' ', text) # Remove all non-word characters
+    text = re.sub(r'\d+', '', text) # Remove numbers
+    text = re.sub(r'\s+', ' ', text) # Replace multiple spaces with a single space
+    return text
 
 @app.route('/')
 def index():
@@ -20,11 +30,17 @@ def predict():
     # Get the news input from the request
     news_text = request.form['news_text']
 
-    # Reshape the input text to 2D (1 sample with 1 feature)
-    input_data = np.array([news_text]).reshape(1, -1)  # Reshaping to 2D array
+    # Preprocess the news text
+    cleaned_news_text = preprocess_text(news_text)
+
+    # Transform the input text using the loaded TF-IDF vectorizer
+    vectorized_news = vectorizer.transform([cleaned_news_text])
 
     # Make the prediction using the model
-    prediction = model.predict(input_data)
+    prediction = model.predict(vectorized_news)
+
+    print(f"Prediction value: {prediction[0]}")
+    print(f"Prediction type: {type(prediction[0])}")
 
     # Return the result as JSON
     return jsonify({'prediction': prediction[0]})
